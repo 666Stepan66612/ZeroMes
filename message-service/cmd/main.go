0872ee7d94a6a75747c423739b06b4460ed8a3f9
Lozovoy Stepan <lskuku1211@gmail.com>
2026-02-23 10:52:42 +0300

    main & Dockerfile & gitignore

---
+++ message-service/cmd/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"log"
+	pb "message-service/gen/messagepb"
+	"message-service/internal/messaging/repository"
+	"message-service/internal/messaging/service"
+	"message-service/internal/messaging/transport"
+	"message-service/pkg/kafka"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	databaseURL := os.Getenv("POSTGRES_URL")
+	if databaseURL == "" {
+		log.Fatal("DATABASE_URL is required")
+	}
+
+	kafkaBrokers := os.Getenv("KAFKA_BROKERS")
+	if kafkaBrokers == "" {
+		log.Fatal("KAFKA_BROKERS is required")
+	}
+
+	grpcPort := os.Getenv("GRPC_PORT")
+	if grpcPort == "" {
+		grpcPort = "50051"
+	}
+
+	ctx := context.Background()
+
+	pgPool, err := pgxpool.New(ctx, databaseURL)
+	if err != nil {
+		log.Fatal("Failed to connect to Database:", err)
+	}
+	defer pgPool.Close()
+
+	if err := pgPool.Ping(ctx); err != nil {
+		log.Fatal("Failed to ping Database", err)
+	}
+	log.Println("Connected to Database")
+
+	kafkaProducer := kafka.NewProducer([]string{kafkaBrokers}, "messages.sent")
+	defer kafkaProducer.Close()
+	log.Println("Kafka producer initialized")
+
+	messageRepo := repository.NewPostgresRepository(pgPool)
+
+	messageService := service.NewMessageService(messageRepo, kafkaProducer)
+
+	grpcServer := grpc.NewServer()
+	grpcHandler := transport.NewGRPCHandler(messageService)
+	pb.RegisterMessageServiceServer(grpcServer, grpcHandler)
+
+	listener, err := net.Listen("tcp", ":"+grpcPort)
+	if err != nil {
+		log.Fatal("Failed to listen: ", err)
+	}
+
+	go func() {
+		log.Println("gRPC server listening on: ", grpcPort)
+		if err := grpcServer.Serve(listener); err != nil {
+			log.Fatal("Failer to server: ", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Gracefull shutdown start")
+
+	grpcServer.GracefulStop()
+
+	kafkaProducer.Close()
+
+	pgPool.Close()
+
+	log.Println("Server stopped")
+}
