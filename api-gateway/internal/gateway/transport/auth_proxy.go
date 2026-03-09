f8aa6f665c2b15f87d20760fdcb5f0d3c8b15951
Lozovoy Stepan <lskuku1211@gmail.com>
2026-03-09 13:46:28 +0300

    auth proxy

---
+++ api-gateway/internal/gateway/transport/auth_proxy.go
@@ -0,0 +1,27 @@
+package transport
+
+import (
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AuthProxy struct {
+	proxy *httputil.ReverseProxy
+}
+
+func NewAuthProxy(authServiceURL string) (*AuthProxy, error) {
+	target, err := url.Parse(authServiceURL)
+	if err != nil {
+		return nil, err
+	}
+	return &AuthProxy{
+		proxy: httputil.NewSingleHostReverseProxy(target),
+	}, nil
+}
+
+func (p *AuthProxy) Register(c *gin.Context) { p.proxy.ServeHTTP(c.Writer, c.Request)}
+func (p *AuthProxy) Login(c *gin.Context) { p.proxy.ServeHTTP(c.Writer, c.Request)}
+func (p *AuthProxy) Refresh(c *gin.Context) { p.proxy.ServeHTTP(c.Writer, c.Request)}
+func (p *AuthProxy) Logout(c *gin.Context) { p.proxy.ServeHTTP(c.Writer, c.Request)}
