da5175d30e202ac5330cd4772036c1ba56b20724
Lozovoy Stepan <lskuku1211@gmail.com>
2026-04-07 14:47:07 +0300

    home page update

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,3 +2,4 @@ export { RegisterPage } from './RegisterPage';
 export { LoginPage } from './LoginPage';
 export { ChangePasswordPage } from './ChangePasswordPage';
 export { ChatsPage } from './ChatsPage';
+export { HomePage } from "./HomePage";
